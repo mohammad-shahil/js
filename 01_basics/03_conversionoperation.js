20d4781512ca3db1374de944596dcaf3e5456f39
mohammad-shahil <mohammadshahil807@gmail.com>
2025-02-07 17:45:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+//let score=33//NUMBER
+//let score="33a"//NaN
+//let score=null//0
+//let score=undefined. //NaN
+//let score=true//
+//console.log(typeof score);
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//let isLoggedIn=1//true
+//let isLoggedIn="shahil"//true
+//let isLoggedIn=0//false
+//let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn)
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);//33
+console.log(typeof stringNumber);//string
