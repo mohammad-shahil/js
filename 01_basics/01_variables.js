facc7a9a2834cf6053b13380cffb70feb6eda4e6
mohammad-shahil <mohammadshahil807@gmail.com>
2025-02-07 17:09:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144553
+var accountEmail="shahil@gmail.com"
+let accountPassword="12345"
+accountCity="jaipur"
+//accountId=2
+console.log(accountId);
+accountEmail="shaham@gmail.com"
+accountPassword="12233"
+accountCity="mangalore"
+//we can use console.table to print all variables name together instead of using console.log for each variable name
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)// we are using nodejs,not browser
+console.log(3+3)
+console.log("shahil")
+
+let name="shahil"
+let age=18
+let isLoggedIn=false
+//bigInt=>if the number is too big
+//string=>""
+//boolean=>true/false
+//null=>standalone value,can be assigned directly
+//undefined=>
+    //symbol=>unique
+
+//null is a object
+console.log(typeof age); //prints the type of datatype
+console.log(typeof null);//output will be object ,null is a object
+console.log(typeof undefined);//output will be undefined itself
